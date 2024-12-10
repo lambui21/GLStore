17c418dde2376f8132e9fdeb45f8a71d3ed4652c
M S I <buigialam212003@gmail.com>
2024-12-10 19:08:59 +0700

    first commit

---
+++ app/src/main/java/com/pro/electronic/utils/ConfigEmail.java
@@ -0,0 +1,15 @@
+package com.pro.electronic.utils;
+
+public class ConfigEmail {
+    public static final String SENDER_EMAIL = "foododersystem2023@gmail.com";
+    public static final String PASSWORD_SENDER_EMAIL = "lkejxoahddhaacos";
+    public static final String RECEIVER_EMAIL = "mixproduction29932@gmail.com";
+
+    public static final String MAIL_HOST_KEY = "mail.smtp.host";
+    public static final String MAIL_POST_KEY = "mail.smtp.port";
+    public static final String MAIL_SSL_KEY = "mail.smtp.ssl.enable";
+    public static final String MAIL_AUTH_KEY = "mail.smtp.auth";
+
+    public static final String MAIL_HOST_VALUE = "smtp.gmail.com";
+    public static final String MAIL_POST_VALUE = "465";
+}
